e193cef0da0e3c883ce0654971e21d5adf037553
bryce <brycekbargar@gmail.com>
2020-11-26 11:39:26 -0600

    Refactor the tests to run in parallel

--- route_test.go
+++ route_test.go
@@ -5,42 +5,97 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func matches(t *testing.T, r Route, u *url.URL) {
+func matches(t *testing.T, r Route, us string) {
 	t.Helper()
-	t.Logf("route: %s == url: %s", r.Path, u.Path)
-	assert.True(t, r.handles(u))
+
+	u, err := url.Parse(us)
+	require.NoError(t, err)
+	assert.True(t, r.handles(u), "because route: %s matches url: %s", r.Path, u.Path)
 }
 
-func doesnotmatch(t *testing.T, r Route, u *url.URL) {
+func doesnotmatch(t *testing.T, r Route, us string) {
 	t.Helper()
-	t.Logf("route: %s != url: %s", r.Path, u.Path)
-	assert.False(t, r.handles(u))
+
+	u, err := url.Parse(us)
+	require.NoError(t, err)
+	assert.False(t, r.handles(u), "because route: %s does not match url: %s", r.Path, u.Path)
 }
 
-func TestRoute_handlesexact(t *testing.T) {
+func TestRoute_exact(t *testing.T) {
+	testCases := []struct {
+		Name string
+		Url  string
+		Test func(*testing.T, Route, string)
+	}{
+		{
+			"Short Path",
+			"http://localhost/some/path",
+			matches,
+		},
+		{
+			"Short Path with query parameters",
+			"http://localhost/some/path?with=two&query=params",
+			matches,
+		},
+		{
+			"Short Path with trailing slash",
+			"http://localhost/some/path/",
+			matches,
+		},
+		{
+			"Long Path",
+			"http://localhost/some/path/that/goes/on",
+			doesnotmatch,
+		},
+	}
+
 	route := Route{"/some/path", nil}
-
-	url, _ := url.Parse("http://localhost/some/path")
-	matches(t, route, url)
-
-	url, _ = url.Parse("http://localhost/some/path/")
-	matches(t, route, url)
-
-	url, _ = url.Parse("http://localhost/some/path/that/goes/on")
-	doesnotmatch(t, route, url)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			tc.Test(t, route, tc.Url)
+		})
+	}
 }
 
-func TestRoute_handletrailingslash(t *testing.T) {
+func TestRoute_trailingslash(t *testing.T) {
+	testCases := []struct {
+		Name string
+		Url  string
+		Test func(*testing.T, Route, string)
+	}{
+		{
+			"Short Path",
+			"http://localhost/some/path",
+			matches,
+		},
+		{
+			"Short Path with query parameters",
+			"http://localhost/some/path?with=two&query=params",
+			matches,
+		},
+		{
+			"Short Path with trailing slash",
+			"http://localhost/some/path/",
+			matches,
+		},
+		{
+			"Long Path",
+			"http://localhost/some/path/that/goes/on",
+			matches,
+		},
+	}
+
 	route := Route{"/some/path/", nil}
-
-	url, _ := url.Parse("http://localhost/some/path")
-	matches(t, route, url)
-
-	url, _ = url.Parse("http://localhost/some/path/")
-	matches(t, route, url)
-
-	url, _ = url.Parse("http://localhost/some/path/that/goes/on")
-	matches(t, route, url)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			tc.Test(t, route, tc.Url)
+		})
+	}
 }
